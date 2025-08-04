9e3be7567d40c1ddd7e31ea723dab5f0b7e872b1
felipejulianes <136484753+felipejulianes@users.noreply.github.com>
2025-08-04 16:33:31 -0300

    Create embajadores.py

---
+++ embajadores.py
@@ -0,0 +1,32 @@
+embajadores = {
+    "general": {
+        "nombre": "General",
+        "imagen": "VRCFotoVieja.jpg",
+        "links": {
+            "20000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=c3424aaac71245d0bddb9302b4355464",
+            "40000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=8a3740707239470689d99cd32689fcc8",
+            "60000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=bbd09e30e0fe40fd97e5826c9fbdd927",
+            "libre": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=3387290f88a6492ca06d340f6a8e68d5"
+        }
+    },
+    "marcos": {
+        "nombre": "Marcos",
+        "imagen": "VRCFotoVieja.jpg",
+        "links": {
+            "20000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=3edf23beb4ac42b7b18d83e45c21eca7",
+            "40000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=66d70a60ac304e478f97d294feb4bcdf",
+            "60000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=3e7c560d62804fd29bc355b9096b186f",
+            "libre": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=cf4c3778ee6942aea7f4333bde705f61"
+        }
+    },
+    "jorge": {
+        "nombre": "Jorge",
+        "imagen": "VRCFotoVieja.jpg",
+        "links": {
+            "20000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=8f16a0eceadd46499830d5dbfe32d5a0",
+            "40000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c5e114b8e9a45c38aab4976fa1ef948",
+            "60000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=aadc55e4fec54bbabdce8601de1708b0",
+            "libre": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=1bd514a1e96b48629ef7e85d32b9552d"
+        }
+    }
+}
