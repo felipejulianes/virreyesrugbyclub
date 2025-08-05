b8bfdeec88513f81fc3b44594d8253231a1cd5bc
felipejulianes <136484753+felipejulianes@users.noreply.github.com>
2025-08-05 13:29:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -56,11 +56,10 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.markdown("""
-    <div style="text-align: center; margin-top: -20px; margin-bottom: 10px;">
-        <img src="logovrc.png" width="250">
-    </div>
-""", unsafe_allow_html=True)
+
+col = st.columns(3)[1]  # columna del medio
+with col:
+    st.image("logovrc.png", width=150)
 
 # Título central
 st.markdown("<h1 style='text-align: center;'>SOCIO SOLIDARIO</h1>", unsafe_allow_html=True)
