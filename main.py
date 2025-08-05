0d4aee846d6816b02c61280ec306445dad063964
felipejulianes <136484753+felipejulianes@users.noreply.github.com>
2025-08-05 13:40:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -60,9 +60,7 @@ col = st.columns(3)[1]
 with col:
     st.image("logovrc.png", width=220)
 
-st.markdown("<div style='margin-top: -100px;'></div>", unsafe_allow_html=True)
-
-st.markdown("<h1 style='text-align: center;'>SOCIO SOLIDARIO</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; margin-top: -40px;'>SOCIO SOLIDARIO</h1>", unsafe_allow_html=True)
 
 
 # Mensaje principal
